13fc0564ac82936817d2fba6571119272b75a5aa
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-07-15 11:51:03 +0200

    + cactus

--- src/lib/components/section/Section.svelte
+++ src/lib/components/section/Section.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+	export let fullwidth = false;
+</script>
+
 <div class="w-full flex justify-center items-center">
-	<div class="flex flex-col max-w-6xl">
+	<div class="flex flex-col w-full {fullwidth ? '' : 'max-w-6xl'}">
 		<slot />
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import LogoInstagram from 'carbon-icons-svelte/lib/LogoInstagram.svelte';
 	import LogoGithub from 'carbon-icons-svelte/lib/LogoGithub.svelte';
+	import Checkmark from 'carbon-icons-svelte/lib/Checkmark.svelte';
+
 	import Banner from '$lib/components/banner/Banner.svelte';
 	import Section from '$lib/components/section/Section.svelte';
 	import Card from '$lib/components/card/Card.svelte';
@@ -32,7 +34,7 @@
 			</Banner>
 		</Section>
 		<Section>
-			<div class="my-12">
+			<div class="my-24">
 				<h2 class="text-5xl">Czym się zajmuję?</h2>
 			</div>
 			<div class="w-full flex gap-8">
@@ -77,6 +79,44 @@
 						<ArrowRight size={32} />
 					</a>
 				</Card>
+			</div>
+		</Section>
+		<Section>
+			<div class="w-full flex gap-8 mt-36">
+				<div class="flex-1 p-4">
+					<img src="/assets/kaktus.jpg" alt="kaktus" />
+				</div>
+				<div class="flex-1 p-4 flex flex-col justify-center">
+					<h3 class="text-5xl">Mój Kaktus</h3>
+					<p class="mt-12">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam libero maxime quas
+						atque nobis nostrum, optio quae magnam ratione officia maiores hic error cum deleniti
+						veritatis eos fugiat mollitia, perferendis voluptas. Neque nihil earum repudiandae
+						distinctio, adipisci quo blanditiis esse.
+					</p>
+					<p class="mt-4">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur iste blanditiis
+						fuga autem natus aut reprehenderit quasi suscipit quisquam. Earum.
+					</p>
+					<ul class="mt-4">
+						<li class="align-middle">
+							<Checkmark size={24} class="inline mr-2 text-secondary-500" /> Podlewanie kaktusa jest
+							mega ważne.
+						</li>
+						<li class="align-middle">
+							<Checkmark size={24} class="inline mr-2 text-secondary-500" /> Nasz zespół podleje ci kaktusa
+							teraz.
+						</li>
+						<li class="align-middle">
+							<Checkmark size={24} class="inline mr-2 text-secondary-500" /> Kto nie ma kaktusa nie ma
+							igieł z kaktusa.
+						</li>
+						<li class="align-middle">
+							<Checkmark size={24} class="inline mr-2 text-secondary-500" /> Wytrzymałość kaktusa zależy
+							od gleby.
+						</li>
+					</ul>
+				</div>
 			</div>
 		</Section>
 	</div>
