462e76dd339c37d73e194e8466a9ead0eaa6b521
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-07-16 16:45:51 +0200

    fix style

--- src/lib/components/social-widget/SocialWidget.svelte
+++ src/lib/components/social-widget/SocialWidget.svelte
@@ -8,11 +8,11 @@
 	<div class="w-1/3 p-4 border-r-4 border-surface-500">
 		<slot name="image" />
 	</div>
-	<div class="w-2/3 p-4 flex justify-center items-center flex-col">
-		<h3 class="text-3xl">
+	<div class="w-2/3 p-8 flex justify-center items-center flex-col">
+		<h3 class="text-xl xl:text-3xl">
 			<slot name="title" />
 		</h3>
-		<p class="text-xl mt-2 text-primary-600">
+		<p class="text-lg xl:text-xl mt-1 xl:mt-2 text-primary-600">
 			<slot name="subtitle" />
 		</p>
 	</div>
